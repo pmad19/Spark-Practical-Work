d0da5d1c0672148416b8378eedff212b79fe00de
francisco.madrigal <francisco.madrigal@vipera.com>
2023-12-29 11:44:11 +0100

    Add new preprocessing methods

--- src/main/python/core/jobs/extract/extract_job.py
+++ src/main/python/core/jobs/extract/extract_job.py
@@ -17,16 +17,15 @@ class Extract:
         schema: T.StructType = self.get_schema()
         df: DataFrame = self.spark.read.csv(self.inputFilePath, header=True, schema=schema)
         df_processed: DataFrame = self.drop_forbidden_variables(df)
-        
-        df_processed = df_processed.filter(df_processed['Cancelled'] == 0)
+        df_processed: DataFrame = self.drop_cancelled_fields(df_processed)
+        df_processed: DataFrame = self.format_time_variables(df_processed)
+        df_processed: DataFrame = self.create_date_time(df_processed)
+        df_processed: DataFrame = self.add_season_column(df_processed)
+        df_processed = df_processed.drop(*['DayofMonth', 'Year', 'Month'])
+        df_processed: DataFrame = self.add_day_time_columns(df_processed)
 
-        df_processed = df_processed.withColumn('DepTime', F.when(F.length(F.col('DepTime')) < 4 , F.concat(F.lit("0"), 'DepTime')).otherwise(F.col('DepTime')))\
-            .withColumn('CRSDepTime', F.when(F.length(F.col('CRSDepTime')) < 4 , F.concat(F.lit("0"), 'CRSDepTime')).otherwise(F.col('CRSDepTime')))\
-            .withColumn('CRSArrTime', F.when(F.length(F.col('CRSArrTime')) < 4 , F.concat(F.lit("0"), 'CRSArrTime')).otherwise(F.col('CRSArrTime')))\
-            .withColumn('DepTime',  F.concat_ws(":", F.substring(F.col('DepTime'), 1, 2), F.substring(F.col('DepTime'), 3, 4)))\
-            .withColumn('CRSDepTime', F.concat_ws(":", F.substring(F.col('CRSDepTime'), 1, 2), F.substring(F.col('CRSDepTime'), 3 , 4)))\
-            .withColumn('CRSArrTime', F.concat_ws(":", F.substring(F.col('CRSArrTime'),1 , 2), F.substring(F.col('CRSArrTime'), 3, 4))) 
-        
+        #df_processed = df_processed.distinct()
+
         df_processed.show()
         self.logger.info('Extract Job: END')
     
@@ -72,3 +71,44 @@ class Extract:
         return df.drop(*['ArrTime', 'ActualElapsedTime', 'AirTime', 'TaxiIn',
                                 'Diverted', 'CarrierDelay', 'WeatherDelay', 'NASDelay',
                                 'SecurityDelay', 'LateAircraftDelay'])
+    
+    def drop_cancelled_fields(self, df: DataFrame) -> DataFrame:
+        return df.filter(df['Cancelled'] == 0)\
+                .drop(*['CancellationCode', 'Cancelled'])
+    
+    def format_time_variables(self, df: DataFrame) -> DataFrame:
+        return df.withColumn('DepTime', F.when(F.length(F.col('DepTime')) < 4 , F.concat(F.lit("0"), 'DepTime')).otherwise(F.col('DepTime')))\
+            .withColumn('CRSDepTime', F.when(F.length(F.col('CRSDepTime')) < 4 , F.concat(F.lit("0"), 'CRSDepTime')).otherwise(F.col('CRSDepTime')))\
+            .withColumn('CRSArrTime', F.when(F.length(F.col('CRSArrTime')) < 4 , F.concat(F.lit("0"), 'CRSArrTime')).otherwise(F.col('CRSArrTime')))\
+            .withColumn('DepTime',  F.concat_ws(":", F.substring(F.col('DepTime'), 1, 2), F.substring(F.col('DepTime'), 3, 4)))\
+            .withColumn('CRSDepTime', F.concat_ws(":", F.substring(F.col('CRSDepTime'), 1, 2), F.substring(F.col('CRSDepTime'), 3 , 4)))\
+            .withColumn('CRSArrTime', F.concat_ws(":", F.substring(F.col('CRSArrTime'),1 , 2), F.substring(F.col('CRSArrTime'), 3, 4))) 
+    
+    def create_date_time(self, df: DataFrame) -> DataFrame:
+        return df.withColumn('Date', F.concat_ws("/", F.col('Year'), F.col('Month'), F.col('DayofMonth')))
+    
+    def add_season_column(self, df: DataFrame) -> DataFrame:
+        return df.withColumn('Season', F.when((F.col('Month') == 12) | (F.col('Month') <= 2), F.lit("Winter"))\
+                             .when((F.col('Month') > 2) | (F.col('Month') <= 5), F.lit("Spring"))\
+                             .when((F.col('Month') > 5) | (F.col('Month') <= 8), F.lit("Summer"))\
+                             .otherwise(F.lit("Autumn")))
+    
+    def add_day_time_columns(self, df: DataFrame) -> DataFrame:
+        # AQUI REVISAR A VER COMO FUNCIONA MEJOR EL MODELO SI CON VARIABLE DEPTIME X3 O CON VARIABLES DEPTIME CRSDEPTIME Y CRSARRTIME
+        df = df.withColumn('DepTime', F.date_format(F.col('DepTime'), "HH:mm"))\
+                .withColumn('CRSDepTime', F.date_format(F.col('CRSDepTime'), "HH:mm"))\
+                .withColumn('CRSArrTime', F.date_format(F.col('CRSArrTime'), "HH:mm"))\
+        
+        df = df.withColumn('DepTime', F.when((F.col('DepTime') > "06:00") & (F.col('DepTime') <= "12:00"), "Morning")
+                               .when((F.col('DepTime') > "12:00") & (F.col('DepTime') <= "17:00"), "Afternoon")
+                               .when((F.col('DepTime') > "17:00") & (F.col('DepTime') <= "20:00"), "Evening")
+                               .otherwise("Night"))\
+                .withColumn('CRSDepTime', F.when((F.col('CRSDepTime') > "06:00") & (F.col('CRSDepTime') <= "12:00"), "Morning")
+                               .when((F.col('CRSDepTime') > "12:00") & (F.col('CRSDepTime') <= "17:00"), "Afternoon")
+                               .when((F.col('CRSDepTime') > "17:00") & (F.col('CRSDepTime') <= "20:00"), "Evening")
+                               .otherwise("Night"))\
+                .withColumn('CRSArrTime', F.when((F.col('CRSArrTime') > "06:00") & (F.col('CRSArrTime') <= "12:00"), "Morning")
+                               .when((F.col('CRSArrTime') > "12:00") & (F.col('CRSArrTime') <= "17:00"), "Afternoon")
+                               .when((F.col('CRSArrTime') > "17:00") & (F.col('CRSArrTime') <= "20:00"), "Evening")
+                               .otherwise("Night"))
+        return df
